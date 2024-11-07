7bc0f4168efddd59855b08f51b86dd7455b09ff5
0r_gh0 <arghacious@gmail.com>
2024-11-07 04:38:34 +0000

    29 Bits + Makefile

--- FINAL/main.c
+++ FINAL/main.c
@@ -3,12 +3,11 @@
 #include "read.h"
 #include "add_sub.h"
 
-
 int main(){
 	struct gf num1, num2, result, pad0, pad1;
 	makeInt(&num1,9);
 	makeInt(&num2,9);
-
+/*
     RAND_bytes((unsigned char*) &num1.num[0], 4);
     RAND_bytes((unsigned char*) &num1.num[1], 4);
     RAND_bytes((unsigned char*) &num1.num[2], 4);
@@ -26,15 +25,18 @@ int main(){
     RAND_bytes((unsigned char*) &num2.num[5], 4);
     RAND_bytes((unsigned char*) &num2.num[6], 4);
     RAND_bytes((unsigned char*) &num2.num[7], 4);
+*/
+    fill29(&num1);
+    fill29(&num2);
 
     printf("\nNumber 1 : ");
     for(int i = num1.len - 1; i >= 0; i--)
-        printf("%08llx", num1.num[i]);
+        printf("%016llx \n", num1.num[i]);
 
     printf("\nNumber 2 : ");
 	for(int i = num2.len - 1; i >= 0; i--)
-        printf("%08llx", num2.num[i]);
-
+        printf("%016llx \n", num2.num[i]);
+/*
     pad1 = sub(&num1, &num2);
     printf("\nNumber 3 : ");
 	for(int i = pad1.len - 1; i >= 0; i--)
@@ -45,7 +47,7 @@ int main(){
 	for(int i = pad1.len - 1; i >= 0; i--)
         printf("%010llx\n", pad1.num[i]);
 	printf("\n");
-
+*/
     // Pad Testing
     // pad0 = pad(&num2,12);
     // printf("\n Number 3 : ");
@@ -58,4 +60,4 @@ int main(){
     // for(int i = num1.len - 1; i >= 0; i--)
     //     printf("%08llx\n", num1.num[i]);
     // return 0;
-} 
+}
--- FINAL/read.h
+++ FINAL/read.h
@@ -1,11 +1,13 @@
 // read.h
 #ifndef READ_H  // Start include guard
-#define READ_H  
+#define READ_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/rand.h>
 
+typedef unsigned char u8;
 typedef unsigned int u32;
 typedef unsigned long long int u64;
 
@@ -21,5 +23,50 @@ void makeInt(struct gf* in, int SIZE){
 	memset(in->num, 0, SIZE * sizeof(u64));
 	in->len = SIZE;
 }
-//https://github.com/0r-gh0/GALOIS
+
+// Function to push random 29-bit data into the struct
+/*
+void fill29(struct gf* in) {
+    for (int i = 0; i < in->len; i++) {
+        u8 buffer[4]; // 29 bits can fit into 4 bytes
+        if (RAND_bytes(buffer, 4) != 1) {
+            fprintf(stderr, "RAND_bytes failed\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Mask the top 3 bits to ensure only 29 bits are set
+        u64 randomValue = ((u64)(buffer[0] & 0x1F) << 24) | ((u64)buffer[1] << 16) | ((u64)buffer[2] << 8) | (u64)buffer[3];
+
+        randomValue &= 0x1FFFFFFF; // Ensure only 29 bits are kept
+        in->num[i] = randomValue;
+    }
+}
+*/
+
+// Function to push random 29-bit data into the first 8 blocks and 24-bit data into the last block
+void fill29(struct gf* in) {
+    for (int i = 0; i < in->len; i++) {
+        u8 buffer[4]; // Buffer to hold random bytes
+        if (RAND_bytes(buffer, 4) != 1) {
+            fprintf(stderr, "RAND_bytes failed\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if (i < 8) { // For the first 8 blocks
+            u64 randomValue = ((u64)(buffer[0] & 0x1F) << 24) |
+                              ((u64)buffer[1] << 16) |
+                              ((u64)buffer[2] << 8) |
+                              (u64)buffer[3];
+            randomValue &= 0x1FFFFFFF; // Mask to ensure only 29 bits
+            in->num[i] = randomValue;
+        } else { // For the last block (24 bits)
+            u64 randomValue = ((u64)buffer[0] << 16) |
+                              ((u64)buffer[1] << 8) |
+                              (u64)buffer[2];
+            randomValue &= 0xFFFFFF; // Mask to ensure only 24 bits
+            in->num[i] = randomValue;
+        }
+    }
+}
+
 #endif  // End include guard
