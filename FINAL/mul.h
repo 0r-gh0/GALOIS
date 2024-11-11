f92c993f93fe8aec46663dc05d31e7d3f96eaa4e
0r_gh0 <arghacious@gmail.com>
2024-11-11 09:48:59 +0000

    FIXED BLOCK

---
+++ FINAL/mul.h
@@ -0,0 +1,6 @@
+// mul.h
+#ifndef MUL_H	// Start include guard
+#define MUL_H
+
+
+#endif MUL_H	// END
--- FINAL/read.h
+++ FINAL/read.h
@@ -24,24 +24,12 @@ void makeInt(struct gf* in, int SIZE){
 	in->len = SIZE;
 }
 
-// Function to push random 29-bit data into the struct
-/*
-void fill29(struct gf* in) {
-    for (int i = 0; i < in->len; i++) {
-        u8 buffer[4]; // 29 bits can fit into 4 bytes
-        if (RAND_bytes(buffer, 4) != 1) {
-            fprintf(stderr, "RAND_bytes failed\n");
-            exit(EXIT_FAILURE);
-        }
-
-        // Mask the top 3 bits to ensure only 29 bits are set
-        u64 randomValue = ((u64)(buffer[0] & 0x1F) << 24) | ((u64)buffer[1] << 16) | ((u64)buffer[2] << 8) | (u64)buffer[3];
-
-        randomValue &= 0x1FFFFFFF; // Ensure only 29 bits are kept
-        in->num[i] = randomValue;
+// Function to populate the struct with binary numbers from an array of strings
+void fill(struct gf* numStruct, char binaryStrings[][65], int numBlocks) {
+    for (int i = 0; i < numBlocks; i++) {
+        numStruct->num[i] = strtoull(binaryStrings[i], NULL, 2);
     }
 }
-*/
 
 // Function to push random 29-bit data into the first 8 blocks and 24-bit data into the last block
 void fill29(struct gf* in) {
